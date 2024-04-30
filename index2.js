5b8440398fc59f248cf6f71a9fdeed022e0873ca
Vladimir Chandi <ljemri184@gmail.com>
2024-04-30 12:43:48 -0500

    Ejemplo suma y resta

--- index2.js
+++ index2.js
@@ -7,11 +7,52 @@ app.get ('/',function (req, res){
  res.send('Hello word')
 console.log("El servicio esta corriendo");
 
-})
+});
 //2.-Servicio web de tipo get
 app.get('/a',function(req,res){
 res.send('VOCAL')
 
-})
+});
+
+//3.- Definir con un paramewtro en la url
+app.get('/suma/:n1',(req,res)=>{
+    const num1=req.params.n1;
+    var tot=11+num1;
+    res.send(tot)
+    
+    });
+
+        //4.- Definir con un paramewtro en la url en json
+app.get('/suma/:n1',(req,res)=>{
+    const num1=parseInt(req.params.n1);
+    var tot=11+num1;
+    const json={
+        resultado:tot
+    };
+
+    res.json(jason)
+    
+    });
+    
+    //5.- Suma de dos numeros usando parametros Url
+    app.get('/suma/:n1/:n2', (req, resp) => {
+        const n1 = parseInt(req.params.n1);
+        const n2 = parseInt(req.params.n2);
+        var suma = n1 + n2;
+        resp.send(suma + "");
+    });
+
+//6.-Se captura de la url pero no esta definido el parametro
+app.get('/nombre',(req,res)=>{
+    //Me ayuda a obtener informacion no establecida en la url
+const datoObtener=req.query.name;
+res.send('Hola'+' '+ datoObtener)
+
+});
+
+//7.---------------------
+
+
+
 //SERVICIO DESDE EL PUERTO 3000
 app.listen(3000)
