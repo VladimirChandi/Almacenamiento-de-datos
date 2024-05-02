56ea0b98c40b3b2539692bde396659484f4f16b2
Vladimir Chandi <ljemri184@gmail.com>
2024-05-02 11:45:35 -0500

    Ciclos For

--- index2.js
+++ index2.js
@@ -64,7 +64,7 @@ var sumatot = num1 + num2;
 });
 
 //9.- Resta sin parametros y jhon
-//url http:localhost:3000/restar?n11=20&n22=10
+//url http:localhost:3000/resta?n11=20&n22=10
 app.get('/resta',(req,res)=>{
     const num1 = parseInt(req.query.n11);
     const num2 = parseInt(req.query.n22);
@@ -80,6 +80,76 @@ app.get('/resta',(req,res)=>{
      const resultado={total:resul};
      res.json(resultado);
 });
+
+//10.- Ejercicio Conejos
+// url : http:localhost:3000/conejos?p=3&nPar=5&nCri=3&tMor=20
+
+app.get('/conejos',(req,res)=>{
+    var pActual=0,pMuere=0,pTotal=0,numCrias=0,parejas=0;
+    //periodo
+    const peri=parseInt(req.query.p);
+    //número de parejas conejos
+    const nParejas=parseInt(req.query.nPar);
+    //número de crías
+    const nCrias=parseInt(req.query.nCri);
+    //tasa de Mortalidad
+    const tMort=parseInt(req.query.tMor);
+
+    for(let i=0;i<=peri;i++){
+        if(i==0){
+        parejas=nParejas;
+        pActual=nParejas*2;
+        pMuere=pActual*tMort/100;
+        pTotal=pActual-pMuere;}
+        else{
+            numCrias=nParejas*nCrias;
+            pActual+=numCrias;
+            pMuere=pActual*tMort/100;
+            pTotal=pActual-pMuere;
+            parejas=pTotal/2;
+        }
+        const resultados={pAnual:pActual,
+                          pMorir:pMuere,
+                          pRestante:pTotal,
+                          nParejas:parejas,
+                          nCrias:numCrias};
+        res.json(resultados);
+    }
+    
+
+});
+//11.- ejercicio 10 Genarando array 
+
+app.get('/conejos', (req, res) => {
+    const resultados = [];
+    
+    const peri = parseInt(req.query.p);
+    const nParejas = parseInt(req.query.nPar);
+    const nCrias = parseInt(req.query.nCri);
+    const tMort = parseInt(req.query.tMor);
+
+    let pActual = nParejas * 2;
+
+    for (let i = 0; i <= peri; i++) {
+        const pMuere = pActual * tMort / 100;
+        const pTotal = pActual - pMuere;
+        const parejas = pTotal / 2;
+        const numCrias = nParejas * nCrias;
+
+        resultados.push({
+            pAnual: pActual,
+            pMorir: pMuere,
+            pRestante: pTotal,
+            nParejas: parejas,
+            nCrias: numCrias
+        });
+
+        pActual = pTotal + numCrias;
+    }
+
+    res.json(resultados);
+});
+
 
 
 //SERVICIO DESDE EL PUERTO 3000
